a2a0c378bd1661a26e6d6bc7214cfa153e2825da
alex11920 <av61799@gmail.com>
2024-07-15 20:33:23 +0600

    responsive working

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,20 +1,48 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import Logo from "/src/assets/logo.svg";
+import { Dropdown } from "@/components/Dropdown";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between my-8">
-      <img src={Logo} alt="logo" />
-      <div className="flex gap-12 items-center">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/browseFields" className="hover:underline">Browse Fields</Link>
-        <Link to="/about" className="hover:underline">About Us</Link>
-        <Button asChild>
-          <Link to="/login">Login</Link>
-        </Button>
+    <>
+      {/* <div className="flex justify-between my-8">
+        <img src={Logo} alt="logo" />
+        <div className="flex gap-12 items-center">
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+          <Link to="/browseFields" className="hover:underline">
+            Browse Fields
+          </Link>
+          <Link to="/about" className="hover:underline">
+            About Us
+          </Link>
+          <Button asChild>
+            <Link to="/login">Login</Link>
+          </Button>
+        </div>
+      </div> */}
+      <div className="flex justify-between my-8">
+        <img src={Logo} alt="logo" />
+
+        <Dropdown/>
+        {/* <div className="flex gap-12 items-center">
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+          <Link to="/browseFields" className="hover:underline">
+            Browse Fields
+          </Link>
+          <Link to="/about" className="hover:underline">
+            About Us
+          </Link>
+          <Button asChild>
+            <Link to="/login">Login</Link>
+          </Button>
+        </div> */}
       </div>
-    </div>
+    </>
   );
 };
 
